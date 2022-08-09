8bd59ba16e88c5ffc79810aade53c44978c88070
Anthony Cella <anthony.cella242@gmail.com>
2022-08-09 11:42:26 -0400

    Created the barebones

--- language/client/src/App.jsx
+++ language/client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './components/title/title';
+import Main from './components/main/main';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Main />
     </div>
   );
 }
---
+++ language/client/src/components/main/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Main() {
+  return (
+    <MainComponent>I am Batman</MainComponent>
+  );
+}
+
+const MainComponent = styled.div`
+  height: 750px;
+  width: 500px;
+`;
+
+export default Main;
