4d444a2182b52b2d8c1817a392e196191cd21873
Anthony Cella <anthony.cella242@gmail.com>
2022-08-11 09:49:20 -0400

    Tweaked the select topic statement

--- language/client/src/components/main/languages/codebox/examplecode.jsx
+++ language/client/src/components/main/languages/codebox/examplecode.jsx
@@ -31,7 +31,7 @@ function ExampleCode({ textContent, selectedLanguage, topic }) {
   }
 
   if (topic === 'Select a topic to begin') {
-    displayedText = topic;
+    displayedText = 'Select a topic by clicking on the autotying text';
   }
 
   return (
